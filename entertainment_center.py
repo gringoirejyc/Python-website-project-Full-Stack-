6dbeb9ba0ee7b90db33c92347d5612686cbdafbb
gringoirejyc <gringoirejyc@gmail.com>
2016-03-06 12:07:52 +0200

    add some comments

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,6 +1,7 @@
 import media
 import fresh_tomatoes
 
+#these 5 object includes info of the movie"
 m1 = media.Movie("Vita e bella",
                  "Life is beautiful",
                  "https://la-vie-est-belle.wikispaces.com/file/view/La_vie_est_belle_affiche.jpg/272769820/363x431/La_vie_est_belle_affiche.jpg",
@@ -23,4 +24,9 @@ m5 = media.Movie("Vita e bella",
                  "https://www.youtube.com/watch?v=hbQvTEV5qXY")
 
 movies = [m1,m2,m3,m4,m5]
+
+# Uses list of movie instances as input to generate an HTML file and open it in the browser.
 fresh_tomatoes.open_movies_page(movies)
+
+#Print the document in the file "media" Movie class
+#print (media.Movie.__doc__)
